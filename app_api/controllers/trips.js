9fef42596a43fbd9a5c98c734e8c4690b687c040
Andrew Robinson <ar2745@gmail.com>
2023-02-14 00:06:13 -0500

    Add Authentication to Express backend and Angular SPA frontend

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Trip = mongoose.model('trips');
+const User = mongoose.model('users');
 
 // GET: /trips - lists all the trips
 const tripsList = async (req, res) => {
@@ -49,6 +50,8 @@ const tripsFindCode = async (req, res) => {
 };
 
 const tripsAddTrip = async (req, res) => {
+    getUser(req, res,
+        (req, res) => {
     Trip
         .create({
             code : req.body.code,
@@ -72,10 +75,14 @@ const tripsAddTrip = async (req, res) => {
                     .json(trip);
             }
         });
+    }
+    )
 } 
 
 const tripsUpdateTrip = async (req, res) => {
     console.log(req.body);
+    getUser(req, res,
+        (req, res) => {
     Trip
         .findOneAndUpdate({ 'code': req.params.tripCode }, {
             code : req.body.code,
@@ -108,7 +115,35 @@ const tripsUpdateTrip = async (req, res) => {
             .status(500) // server error
             .json(err);
         });
+    }
+    )
 }
+
+const getUser = (req, res, callback) => {
+    if (req.payload && req.payload.email) {
+        User
+            .findOne({ email: req.payload.email })
+            .exec((err, user) => {
+                if (!user) {
+                    return res
+                        .status(404)
+                        .json({ "message": "User not found" });
+                }
+                else if (err) {
+                    console.log(err);
+                    return res
+                        .status(404)
+                        .json(err);
+                }
+                callback(req,res, user.name);
+            });
+    }
+    else {
+        return res
+            .status(404)
+            .json({ "message": "User not found" });
+    }
+};
 
 module.exports = {
     tripsList,
--- app_api/models/db.js
+++ app_api/models/db.js
@@ -65,3 +65,4 @@ connect();
 
 // bring in the Mongoose schema
 require('./travlr');
+require('./user');
